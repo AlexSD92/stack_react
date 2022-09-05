9eb146b990c90b39475618a05a25178a403e7f64
AlexSD92 <alex.spara.dominguez@gmail.com>
2022-09-05 19:21:27 +0000

    NewQuestionForm added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import NewQuestionForm from './components/NewQuestionForm';
 
 function App() {
   return (
     <div className="App">
-        <h1>Hello World!</h1>
+        <NewQuestionForm />
     </div>
   );
 }
