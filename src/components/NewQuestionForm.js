1939fb018d32700a0c09e88136b82882af7b1f66
AlexSD92 <alex.spara.dominguez@gmail.com>
2022-09-05 19:21:08 +0000

    NewQuestionForm created to handle question submission

---
+++ src/components/NewQuestionForm.js
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+
+import axios from "axios";
+
+import { API_URL } from "../constants/axios";
+import { Form, Button } from "react-bootstrap";
+// import { useNavigate } from "react-router-dom";
+
+
+function NewQuestionForm() {
+  const [errors, setErrors] = useState({});
+  const [questionData, setQuestionData] = useState({
+    question: "",
+  });
+  const { question } = questionData;
+  // const history = useNavigate();
+
+  const handleChange = (event) => {
+    setQuestionData({
+      ...questionData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData();
+
+    formData.append("question", question);
+
+    try {
+      const { data } = await axios.post("https://stack-drf-api.herokuapp.com/questions/", formData);
+      // history('')
+    } catch (err) {
+      console.log(err);
+      if (err.response?.status !== 401) {
+        setErrors(err.respose?.data)
+      }
+    }
+  };
+
+  return(
+    <div>
+
+      <h1>Question</h1>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group>
+          <Form.Label>Question</Form.Label>
+          <Form.Control 
+            type="text"
+            name="question"
+            value={question}
+            onChange={handleChange}
+          />
+        </Form.Group>
+        <Button>Submit</Button>
+      </Form>
+
+    </div>
+
+
+  )
+
+}
+
+export default NewQuestionForm
