fcb3759999d5d119bbb9e6c68a929ce698e8b350
AlexSD92 <alex.spara.dominguez@gmail.com>
2022-09-11 10:04:05 +0000

    Login.js created added to App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import NewQuestionForm from './components/NewQuestionForm';
 import { Routes, Route } from 'react-router-dom';
 import QuestionList from './components/QuestionList';
 import QuestionDetail from './components/QuestionDetail';
-import AnswerList from './components/AnswerList';
+// import AnswerList from './components/AnswerList';
 import AnswerDetail from './components/AnswerDetail';
 import NewAnswerForm from './components/NewAnswerForm';
 import EditQuestionForm from './components/EditQuestionForm';
@@ -11,27 +11,55 @@ import EditAnswerForm from './components/EditAnswerForm';
 import ProfileList from './components/ProfileList';
 import ProfileDetail from './components/ProfileDetail';
 import Register from './components/Register';
+import Login from './components/Login';
 import Test from './components/Test';
+import { createContext, useEffect, useState } from 'react';
+import axios from 'axios';
+
+
+export const CurrentUserContext = createContext();
+export const SetCurrentUserContext = createContext();
+
 
 function App() {
+  const[currentUser, setCurrentUser] = useState(null)
+
+  const handleMount = async () => {
+    try {
+      const {data} = await axios.get('https://stack-drf-api.herokuapp.com/dj-rest-auth/user/');
+      setCurrentUser(data);
+    } catch(err){
+      console.log(err);
+    }
+  }
+
+  useEffect(() => {
+    handleMount()
+  }, [])
+
   return (
-    <div className="App">
-      <Routes>
-        <Route path="/newquestion" element={<NewQuestionForm />} />
-        <Route path="/editquestion/:id" element={<EditQuestionForm />} />
-        <Route path="/newanswer" element={<NewAnswerForm />} />
-        <Route path="/editanswer/:id" element={<EditAnswerForm />} />
-        <Route path="/questions" element={<QuestionList />} />
-        <Route path="/questions/:id" element={<QuestionDetail />} />
-        <Route path="/answers/:id" element={<AnswerDetail />} />
-        <Route path="/profiles" element={<ProfileList />} />
-        <Route path="/profiles/:id" element={<ProfileDetail />} />
-        {/* <Route path="/profiles/:id" /> */}
-        <Route path="/register" element={<Register />} />
-        <Route path="/test" element={<Test />} />
-        <Route />
-      </Routes>
-    </div>
+    <CurrentUserContext.Provider value={currentUser}>
+      <SetCurrentUserContext.Provider value={setCurrentUser}>
+        <div className="App">
+          <Routes>
+            <Route path="/newquestion" element={<NewQuestionForm />} />
+            <Route path="/editquestion/:id" element={<EditQuestionForm />} />
+            <Route path="/newanswer" element={<NewAnswerForm />} />
+            <Route path="/editanswer/:id" element={<EditAnswerForm />} />
+            <Route path="/questions" element={<QuestionList />} />
+            <Route path="/questions/:id" element={<QuestionDetail />} />
+            <Route path="/answers/:id" element={<AnswerDetail />} />
+            <Route path="/profiles" element={<ProfileList />} />
+            <Route path="/profiles/:id" element={<ProfileDetail />} />
+            {/* <Route path="/profiles/:id" /> */}
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/test" element={<Test />} />
+            <Route />
+          </Routes>
+        </div>
+      </SetCurrentUserContext.Provider>
+    </CurrentUserContext.Provider>
   );
 }
 
---
+++ src/components/Login.js
@@ -0,0 +1,98 @@
+import React, { useState, useContext } from "react";
+import { Form, Alert, Button, Col, Row, Container } from 'react-bootstrap';
+import axios from "axios";
+
+import { Link, useNavigate } from "react-router-dom";
+import { SetCurrentUserContext } from "../App";
+
+
+function Login() {
+  const setCurrentUser = useContext(SetCurrentUserContext)
+
+  const [signInData, setSignInData] = useState({
+    username: "",
+    password: "",
+  });
+  const { username, password } = signInData;
+
+  const [errors, setErrors] = useState({});
+
+  const history = useNavigate();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const {data} = await axios.post("https://stack-drf-api.herokuapp.com/dj-rest-auth/login/", signInData);
+      setCurrentUser(data.user);
+      history('/questions');
+    } catch (err) {
+      setErrors(err.response?.data);
+    }
+  };
+
+  const handleChange = (event) => {
+    setSignInData({
+      ...signInData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  return (
+    <Row>
+      <Col>
+        <Container>
+          <h1>Get Logged In</h1>
+          <Form onSubmit={handleSubmit}>
+            
+            <Form.Group controlId="username">
+              <Form.Label>Username</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Username"
+                name="username"
+                value={username}
+                onChange={handleChange}
+              />
+            </Form.Group>
+            {errors.username?.map((message, idx) => (
+              <Alert key={idx} variant="warning">
+                {message}
+              </Alert>
+            ))}
+
+            <Form.Group controlId="password">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                name="password"
+                value={password}
+                onChange={handleChange}
+              />
+            </Form.Group>
+            {errors.password?.map((message, idx) => (
+              <Alert key={idx} variant="warning">
+                {message}
+              </Alert>
+            ))}
+
+            <Button type="submit">Sign in</Button>
+
+            {errors.non_field_errors?.map((message, idx) => (
+              <Alert key={idx} variant="warning" className="mt-3">
+                {message}
+              </Alert>
+            ))}
+          </Form>
+        </Container>
+        <Container>
+          <Link to="/register">
+            Don't have an account? <span>Sign up now!</span>
+          </Link>
+        </Container>
+      </Col>
+    </Row>
+  );
+}
+
+export default Login;
