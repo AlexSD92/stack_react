0f3149d6e6901cb29d130e41b550348fe1204b9c
AlexSD92 <alex.spara.dominguez@gmail.com>
2022-09-11 12:02:01 +0000

    axiosDefaults.js file created to handle axiosRes and Req, added to App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import NewQuestionForm from './components/NewQuestionForm';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 import QuestionList from './components/QuestionList';
 import QuestionDetail from './components/QuestionDetail';
 // import AnswerList from './components/AnswerList';
@@ -13,8 +13,9 @@ import ProfileDetail from './components/ProfileDetail';
 import Register from './components/Register';
 import Login from './components/Login';
 import Test from './components/Test';
-import { createContext, useEffect, useState } from 'react';
+import { createContext, useEffect, useMemo, useState } from 'react';
 import axios from 'axios';
+import { axiosReq, axiosRes } from './api/axiosDefaults';
 
 
 export const CurrentUserContext = createContext();
@@ -23,10 +24,11 @@ export const SetCurrentUserContext = createContext();
 
 function App() {
   const[currentUser, setCurrentUser] = useState(null)
+  const history = useNavigate()
 
   const handleMount = async () => {
     try {
-      const {data} = await axios.get('https://stack-drf-api.herokuapp.com/dj-rest-auth/user/');
+      const {data} = await axiosRes.get('https://stack-drf-api.herokuapp.com/dj-rest-auth/user/');
       setCurrentUser(data);
     } catch(err){
       console.log(err);
@@ -37,12 +39,54 @@ function App() {
     handleMount()
   }, [])
 
+  useMemo(() => {
+    axiosReq.interceptors.request.use(
+      async (config) => {
+        try {
+          await axios.post('https://stack-drf-api.herokuapp.com/dj-rest-auth/token/refresh/');
+        } catch (err) {
+          setCurrentUser((prevCurrentUser) => {
+            if (prevCurrentUser) {
+              history('/login');
+            }
+            return null;
+          });
+          return config;
+        }
+        return config;
+      },
+      (err) => {
+        return Promise.reject(err);
+      }
+    )
+
+    axiosRes.interceptors.response.use(
+      (response) => response,
+      async (err) => {
+        if (err.response?.status === 401){
+          try{
+            await axios.post('https://stack-drf-api.herokuapp.com/dj-rest-auth/token/refresh/')
+          } catch(err){
+            setCurrentUser(prevCurrentUser => {
+              if (prevCurrentUser){
+                history('/login')
+              }
+              return null
+            })
+          }
+          return axios(err.config)
+        }
+        return Promise.reject(err)
+      }
+    )
+  }, [history])
+
   return (
     <CurrentUserContext.Provider value={currentUser}>
       <SetCurrentUserContext.Provider value={setCurrentUser}>
         <div className="App">
           <Routes>
-            <Route path="/newquestion" element={<NewQuestionForm />} />
+            <Route path="/newquestion" element={<NewQuestionForm currentUser={currentUser} />} />
             <Route path="/editquestion/:id" element={<EditQuestionForm />} />
             <Route path="/newanswer" element={<NewAnswerForm />} />
             <Route path="/editanswer/:id" element={<EditAnswerForm />} />
