4a9711e186d9998c870d05ad5fbd39e0616b6ff2
AlexSD92 <alex.spara.dominguez@gmail.com>
2022-09-25 21:09:39 +0100

    Removed unused imports

--- src/components/questions/QuestionEditOrDetail.js
+++ src/components/questions/QuestionEditOrDetail.js
@@ -1,16 +1,12 @@
 import '../../customcss/questions.css';
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { useCurrentUser } from '../../contexts/CurrentUserContext';
 import QuestionDetail from './QuestionDetail';
-import NewAnswerForm from '../answers/NewAnswerForm';
 import AnswerList from '../answers/AnswerList';
 import { axiosReq } from '../../api/axiosDefaults';
 
 
 function QuestionEditOrDetail() {
-    const currentUser = useCurrentUser();
-
     const params = useParams()
     const [questions, setQuestions] = useState([])
 
@@ -26,11 +22,6 @@ function QuestionEditOrDetail() {
         <div>
 
             <QuestionDetail />
-
-            {currentUser ?
-                <NewAnswerForm /> :
-                null
-            }
 
             <AnswerList questions={questions} />
 
