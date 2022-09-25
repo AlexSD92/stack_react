7feed2c4b5cfe417ca2dd5476bff3bdf16af7bf6
AlexSD92 <alex.spara.dominguez@gmail.com>
2022-09-25 21:20:56 +0100

    Conditional form, disabled if !currentUser

--- src/components/answers/NewAnswerForm.js
+++ src/components/answers/NewAnswerForm.js
@@ -2,10 +2,13 @@ import '../../customcss/answers.css';
 import React, { useState, useEffect } from "react";
 import { Form, Button } from "react-bootstrap";
 import { useNavigate, useParams } from "react-router-dom";
+import { useCurrentUser } from '../../contexts/CurrentUserContext';
 import { axiosReq } from '../../api/axiosDefaults';
+import WarningMessage from '../messages/WarningMessage';
 
 
 function NewAnswerForm() {
+  const currentUser = useCurrentUser();
   const params = useParams();
   const [errors, setErrors] = useState({});
   const [questions, setQuestions] = useState([]);
@@ -52,42 +55,74 @@ function NewAnswerForm() {
   return(
     <div className='parentdivmargin mt-5'>
 
-      <h3 className='left'>Add an answer to this question:</h3>
+          <h3 className='left'>Add an answer to this question:</h3>
 
-      <div className='individualq'>
-              <h2 className=''>{questions.summary}</h2>
-              <p className='m-0'>{questions.question}</p>
+          <div className='individualq'>
+                  <h2 className=''>{questions.summary}</h2>
+                  <p className='m-0'>{questions.question}</p>
 
-              <br/>
+                  <br/>
 
-              <p>This question was created by <strong>{questions.owner}</strong> on <strong>{questions.created_at}</strong>.</p>
-              <p><strong>{questions.owner}</strong> last updated this question on <strong>{questions.updated_at}</strong></p>
-                          
-            </div>
+                  <p>This question was created by <strong>{questions.owner}</strong> on <strong>{questions.created_at}</strong>.</p>
+                  <p><strong>{questions.owner}</strong> last updated this question on <strong>{questions.updated_at}</strong></p>
+                              
+                </div>
 
-      <br/>
+          <br/>
 
-      <Form onSubmit={handleSubmit}>
+      {!currentUser ?
+        <>
+          <Form onSubmit={handleSubmit}>
 
-        <Form.Group>
-          <Form.Control 
-            as='textarea'
-            rows={3}
-            required
-            type="text"
-            name="answer"
-            value={answer}
-            onChange={handleChange}
-          />
-        </Form.Group>
-        
-        <br/>
+            <Form.Group>
+              <Form.Control 
+                disabled
+                placeholder='You need to be logged in to post answers'
+                as='textarea'
+                rows={3}
+                required
+                type="text"
+                name="answer"
+                value={answer}
+                onChange={handleChange}
+              />
+            </Form.Group>
+            
+            <br/>
 
-        <Button variant='success' type="submit">Submit</Button>
+            <Button variant='success' type="submit">Submit</Button>
 
-      </Form>
+          </Form>
+          
+          <br/><br/><hr/><br/><br/>
+        </>
+      :
+        <>
+          <Form onSubmit={handleSubmit}>
+
+            <Form.Group>
+              <Form.Control 
+                as='textarea'
+                rows={3}
+                required
+                type="text"
+                name="answer"
+                value={answer}
+                onChange={handleChange}
+              />
+            </Form.Group>
+            
+            <br/>
+
+            <Button variant='success' type="submit">Submit</Button>
+
+          </Form>
+          
+          <br/><br/><hr/><br/><br/>
+        </>
+
       
-      <br/><br/><hr/><br/><br/>
+      }
 
     </div>
   )
